cbfbc85ec5ca2b2ea869e3481173d0a70a5ec64e
呼吸吸吸 <@192.168.56.1>
2019-08-19 20:46:02 +0800

    注册验证码终极完美不能再改版

--- src/web/CheckCode.java
+++ src/web/CheckCode.java
@@ -15,17 +15,13 @@ public class CheckCode extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String checkCode = request.getParameter("checkcode");
-		System.out.println(checkCode);
 		HttpSession session = request.getSession();
 		String checkcode_session = (String) session.getAttribute("checkcode_session");
+		boolean equals = checkcode_session.equals(checkCode);
 		response.setContentType("text/plain;charset=utf-8");
-		if (checkcode_session.equals(checkCode)) {
-			response.getWriter().write("{\"massage\":\"校验成功\"}");
-			System.out.println(1111);
-		} else {
-			response.getWriter().write("{\"massage\":\"校验失败\"}");
-			System.out.println(2222);
-		}
+		// {"isExists": isExists}
+		response.getWriter().write("{\"isExists\": "+equals+"}");
+	
 		
 	}
 
